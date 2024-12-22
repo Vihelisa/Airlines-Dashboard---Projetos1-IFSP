2478354fec74bf83b734e5cce5081885e77372f4
Vihelisa <fvitoriaelisa@gmail.com>
2024-12-22 20:10:53 -0300

    Update analise_trafego.py

--- views/analise_trafego.py
+++ views/analise_trafego.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title("Análise de Tráfego Aereo")
+from app import df_rotas, df_tam
+
+
+st.write("""
+# Análise de Tráfego Aereo
+## Volume de Passageiros
+Análise do número total de passageiros por rota e período de mês e ano
+""")
+
+
