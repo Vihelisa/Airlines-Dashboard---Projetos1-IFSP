4b95a5e5c48a81e2d8c5f65f0232a7e1e75f4351
Vihelisa <fvitoriaelisa@gmail.com>
2025-01-05 15:17:52 -0300

    filtros e tabela correspondente aos filtros

--- functions/functions.py
+++ functions/functions.py
@@ -53,3 +53,45 @@ def send_email(to_email, password):
         st.error(f"Erro ao enviar email: {e}")
 
     
+def filter_empty_data(select_trafego, select_mes, select_ano):
+    if not select_trafego:
+        select_trafego = 'Todos'
+    if not select_mes:
+        select_mes = 'Todos'
+    if not select_ano:
+        select_ano = 'Todos'
+    return select_trafego, select_mes, select_ano
+
+
+
+def multiselect_mes_ano_option(df_trafego, select_mes, select_ano):
+    if 'Todos' in select_mes and 'Todos' in select_ano:
+        print('Todos tem todos selecionados')
+        df_traf_todos = df_trafego.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['mes', 'ano'])
+        return df_traf_all_filtered
+    
+    elif 'Todos' in select_mes and not 'Todos' in select_ano:
+        print('apenas o ano é diferente')
+        select_ano = [int(item) for item in select_ano]
+        df_traf_todos = df_trafego.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'ano']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['mes'])
+        df_graph = df_traf_todos[df_traf_todos['ano'].isin(select_ano)]
+        return df_graph
+    
+    elif not 'Todos' in select_mes and 'Todos' in select_ano:
+        print('Apenas o mes é diferente')
+        select_mes = [int(item) for item in select_mes]
+        df_traf_todos = df_trafego.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'mes']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['ano'])
+        df_graph = df_traf_all_filtered[df_traf_all_filtered['mes'].isin(select_mes)]
+        return df_graph
+    
+    else:
+        print('O mes e o ano são diferentes')
+        select_mes = [int(item) for item in select_mes]
+        select_ano = [int(item) for item in select_ano]
+        df_traf_todos = df_trafego.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'mes', 'ano']).sum().reset_index()
+        df_graph = df_traf_todos[df_traf_todos['mes'].isin(select_mes)]
+        df_graph = df_traf_todos[df_traf_todos['ano'].isin(select_ano)]
+        return df_graph
--- views/analise_trafego.py
+++ views/analise_trafego.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_echarts import st_echarts
+from st_aggrid import AgGrid
 
 from principal import *
 
@@ -42,4 +44,72 @@ with col2:
 with col3: 
     select_ano = st.multiselect("Selecione o ano", lista_ano)
 
+select_trafego, select_mes, select_ano = filter_empty_data(select_trageto, select_mes, select_ano)
+print(select_trafego, select_mes, select_ano)
+
+if not 'Todos' in select_trageto:
+    lista_df_filtrado = []
+    nomes_separados = [nome for sublista in select_trageto for nome in sublista.split('-')]
+    for num in range(len(nomes_separados)):
+        if num%2 == 0:
+            origem = nomes_separados[num].strip()
+            destino = nomes_separados[num+1].strip()
+            df_filtrado = df_trafego.loc[(df_trafego['aeroporto_de_origem_nome'] == origem) & (df_trafego['aeroporto_de_destino_nome'] == destino)]
+            lista_df_filtrado.append(df_filtrado)
+
+    # Combinar todos os DataFrames filtrados em um único DataFrame 
+    df_filtro_final = pd.concat(lista_df_filtrado).reset_index(drop=True)
+    if 'Todos' in select_mes and 'Todos' in select_ano:
+        df_traf_todos = df_filtro_final.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['mes', 'ano'])
+        AgGrid(df_traf_all_filtered) #Tabela de valores
+    
+    elif 'Todos' in select_mes and not 'Todos' in select_ano:
+        select_ano = [int(item) for item in select_ano]
+        df_traf_todos = df_filtro_final.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'ano']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['mes'])
+        df_graph = df_traf_todos[df_traf_todos['ano'].isin(select_ano)]
+        AgGrid(df_graph) #Tabela de valores
+    
+    elif not 'Todos' in select_mes and 'Todos' in select_ano:
+        select_mes = [int(item) for item in select_mes]
+        df_traf_todos = df_filtro_final.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'mes']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['ano'])
+        df_graph = df_traf_all_filtered[df_traf_all_filtered['mes'].isin(select_mes)]
+        AgGrid(df_graph) #Tabela de valores
+    else:
+        select_mes = [int(item) for item in select_mes]
+        select_ano = [int(item) for item in select_ano]
+        df_traf_todos = df_filtro_final.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'mes', 'ano']).sum().reset_index()
+        df_graph = df_traf_todos[df_traf_todos['mes'].isin(select_mes)]
+        df_graph = df_traf_todos[df_traf_todos['ano'].isin(select_ano)]
+        AgGrid(df_graph)#Tabela de valores
+
+else:
+    if 'Todos' in select_mes and 'Todos' in select_ano:
+        df_traf_todos = df_trafego.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['mes', 'ano'])
+        AgGrid(df_traf_all_filtered) #Tabela de valores
+    
+    elif 'Todos' in select_mes and not 'Todos' in select_ano:
+        select_ano = [int(item) for item in select_ano]
+        df_traf_todos = df_trafego.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'ano']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['mes'])
+        df_graph = df_traf_todos[df_traf_todos['ano'].isin(select_ano)]
+        AgGrid(df_graph) #Tabela de valores
+    
+    elif not 'Todos' in select_mes and 'Todos' in select_ano:
+        select_mes = [int(item) for item in select_mes]
+        df_traf_todos = df_trafego.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'mes']).sum().reset_index()
+        df_traf_all_filtered = df_traf_todos.drop(columns=['ano'])
+        df_graph = df_traf_all_filtered[df_traf_all_filtered['mes'].isin(select_mes)]
+        AgGrid(df_graph) #Tabela de valores
+    else:
+        select_mes = [int(item) for item in select_mes]
+        select_ano = [int(item) for item in select_ano]
+        df_traf_todos = df_trafego.groupby(['aeroporto_de_origem_nome', 'aeroporto_de_destino_nome', 'mes', 'ano']).sum().reset_index()
+        df_graph = df_traf_todos[df_traf_todos['mes'].isin(select_mes)]
+        df_graph = df_traf_todos[df_traf_todos['ano'].isin(select_ano)]
+        AgGrid(df_graph)#Tabela de valores
+    
 
